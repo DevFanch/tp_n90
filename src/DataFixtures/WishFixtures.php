0743e145cdfd2177b03cfdf20c1663756c65161d
devfanch <devfanch44@gmail.com>
2024-08-21 12:25:38 +0200

    M05 TP1-2

---
+++ src/DataFixtures/WishFixtures.php
@@ -0,0 +1,30 @@
+<?php
+
+namespace App\DataFixtures;
+
+use App\Entity\Wish;
+use Doctrine\Bundle\FixturesBundle\Fixture;
+use Doctrine\Persistence\ObjectManager;
+
+class WishFixtures extends Fixture
+{
+    public function load(ObjectManager $manager): void
+    {
+        // Use Faker/php
+        $faker = \Faker\Factory::create('fr_FR');
+
+        // Generate 10 fake wishes
+        for ($i=0; $i < 15; $i++) { 
+            $wish = new Wish();
+            $wish->setTitle('Wish '.$faker->word)
+                ->setAuthor($faker->lastName() .' '. $faker->firstName())
+                ->setDescription($faker->sentences(3, true))
+                ->setPublished($faker->numberBetween(0, 1))
+                ->setDateCreated($faker->dateTimeBetween('-1 years', 'now'));
+
+            $manager->persist($wish);
+        }
+
+        $manager->flush();
+    }
+}
